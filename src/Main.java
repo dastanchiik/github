c808d1c3f3d91a8d5db6ce0f8338065872da1936
dastanchick <dosyafree32@gmail.com>
2022-12-04 16:08:35 +0600

    I added name

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,11 @@ public class Main {
     Parrot parrot = new Parrot("Kesha",2);
     Eagle eagle = new Eagle("Berkut",4);
     Crow crow = new Crow("Kara bash",1);
-        System.out.println(parrot+"\n"+eagle+"\n"+crow);
+        System.out.println(parrot);
+        parrot.flyable();
+        System.out.println(eagle);
+        eagle.flyable();
+        System.out.println(crow);
+        crow.flyable();
     }
 }
